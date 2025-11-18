20bdbca07704482319cb7c81cc8561de31c44a99
Vinz_ <putraalvinandhika@gmail.com>
2025-11-18 14:16:06 +0700

    Add files via upload

---
+++ Puskesmas/Backend/Service/Connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$HostName = "localhost";
+$Username = "root";
+$Password = "";
+$DataBaseName = "puskesmas";
+
+$Connection = new mysqli($HostName , $Username , $Password , $DataBaseName);
+
+
+?>
